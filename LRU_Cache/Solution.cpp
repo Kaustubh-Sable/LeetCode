a8b5e8e2161de1e9e5452643082c97a769fea864
Kaustubh Sable <kaustubhsable05@gmail.com>
2020-03-01 20:01:00 -0500

    Create Solution.cpp

---
+++ LRU_Cache/Solution.cpp
@@ -0,0 +1,96 @@
+struct Node
+{
+    int key;
+    int value;
+    Node *next;
+    Node *prev;
+    
+    Node(int k, int v)
+    {
+        key = k;
+        value = v;
+        next = NULL;
+        prev = NULL;
+    }
+};
+
+class LRUCache {
+    Node *head,*tail;
+    int capacity;
+    unordered_map<int,Node*> umap;
+public:
+    LRUCache(int capacity) {
+        head=NULL;
+        tail=NULL;
+        this->capacity = capacity;
+    }
+    
+    int get(int key) {
+        if(umap.find(key)==umap.end())
+            return -1;
+        else
+        {
+            Node *myNode=umap[key];
+            
+            if(myNode!=head)
+            {
+                Node *mynext=myNode->next;
+                Node *myprev=myNode->prev;
+                
+                myprev->next = mynext;
+                if(mynext)
+                    mynext->prev=myprev;
+                
+                myNode->next=head;
+                
+                if(myNode==tail)
+                    tail = tail->prev;
+                myNode->prev=NULL;
+                head->prev=myNode;
+                head=myNode;
+            }
+            
+            return myNode->value;
+        }
+    }
+    
+    void put(int key, int value) {
+
+        if(umap.find(key)!=umap.end())
+        {
+            Node *temp = umap[key];
+            temp->value=value;
+            get(key);
+        }else if(umap.size()<capacity)
+        {
+            Node *newNode = new Node(key,value);
+            newNode->next = head;
+            if(head)
+            {
+                head->prev = newNode;
+                newNode->next = head;
+            }
+            else
+                tail = newNode;
+            head = newNode;
+            umap[key]=newNode;
+        }
+        else
+        {
+            Node *temp=tail;
+            if(head!=tail)
+                tail = tail->prev;
+            tail->next=NULL;
+            umap.erase(temp->key);
+            
+            put(key,value);
+        }
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
