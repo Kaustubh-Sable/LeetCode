a00c74d56c3ae9fb7d4d5a9132f7a249fae2c2b6
Kaustubh Sable <kaustubhsable05@gmail.com>
2020-05-15 20:09:03 -0400

    Create Solution.cpp

---
+++ May-Leetcoding-challenge/Day15/Solution.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maxSubarraySumCircular(vector<int>& A) {
+        int sum,max=A[0];
+        int i, n=A.size(), temp = A[0], left=0;
+        
+        bool allPositive = A[0]>=0?true:false;
+        
+        for(i=1;i<2*n;i++)
+        {
+            if(A[i%n]<0)
+                allPositive = false;
+            
+            if(i==n && allPositive)
+                return sum;
+            
+            if(A[i%n]+temp>A[i%n] && left!=i%n)
+                sum = A[i%n]+temp;
+            else
+            {
+                if(left==i%n)
+                    i = left + 1;
+                sum = A[i%n];
+                left = i%n;
+            }
+            if(sum>max)
+                max=sum;
+            temp=sum;
+        }
+        return max;
+    }
+};
